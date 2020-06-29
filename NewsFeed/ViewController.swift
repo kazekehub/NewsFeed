7b3b9533d98c920dd7dec3e6f0fe5e116758a5bc
Kazybek <kzhapparkulov@gmail.com>
2020-06-29 15:12:01 +0600

    Initial Commit

---
+++ NewsFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsFeed
+//
+//  Created by Beka Zhapparkulov on 6/29/20.
+//  Copyright © 2020 Kazybek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
